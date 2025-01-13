f276c7276a7cb84e021ad17af3f1eb38ccead59d
Camp4er <camp4ercode@gmail.com>
2025-01-13 18:51:27 +0530

    pdfviewer

--- src/components/SeparateWindows/PdfViewer.tsx
+++ src/components/SeparateWindows/PdfViewer.tsx
@@ -1,14 +1,13 @@
-type PdfViewerProps = {
-    file: string;
-  };
-  
-  const PdfViewer = ({ file }: PdfViewerProps) => {
-    return (
-      <div className="w-full h-full">
-        <embed src={file} type="application/pdf" className="w-full h-full" />
-      </div>
-    );
-  };
-  
-  export default PdfViewer;
-  
+import React from "react";
+
+export default function PDFViewer() {
+  return (
+    <div className="h-full w-full bg-gray-800 text-white flex items-center justify-center">
+      <iframe
+        src="/pdf/PoorvaSaxenaResume6.pdf"
+        className="h-full w-full border-none"
+        title="PDF Viewer"
+      ></iframe>
+    </div>
+  );
+}
