e6f176cf89d31404acac51ca835318294e3523a3
Camp4er <camp4ercode@gmail.com>
2025-02-03 20:22:21 +0530

    pinned apps UI completed

--- src/components/Taskbar/StartButton/StartMenu.tsx
+++ src/components/Taskbar/StartButton/StartMenu.tsx
@@ -2,18 +2,18 @@ import { useEffect } from "react";
 import { FaSearch, FaPowerOff } from "react-icons/fa";
 
 const pinnedApps = [
-  { name: "Edge", icon: "/icons/edge.png" },
-  { name: "Word", icon: "/icons/word.png" },
-  { name: "Excel", icon: "/icons/excel.png" },
-  { name: "PowerPoint", icon: "/icons/powerpoint.png" },
-  { name: "Outlook", icon: "/icons/outlook.png" },
+  { name: "Calculator", icon: "/icons/calculator.png" },
+  { name: "Edge", icon: "/icons/microsoft.png" },
+  { name: "Snakeats", icon: "/icons/snake.png" },
   { name: "Calendar", icon: "/icons/calendar.png" },
-  { name: "Microsoft Store", icon: "/icons/store.png" },
-  { name: "Photos", icon: "/icons/photos.png" },
-  { name: "Spotify", icon: "/icons/spotify.png" },
-  { name: "Xbox", icon: "/icons/xbox.png" },
-  { name: "Instagram", icon: "/icons/instagram.png" },
-  { name: "Messenger", icon: "/icons/messenger.png" },
+  { name: "Google Chrome", icon: "/icons/chrome.svg" },
+  { name: "Visual Studio Code", icon: "/icons/vscode.svg" },
+  { name: "Terminal", icon: "/icons/bash.png" },
+  { name: "Clock", icon: "/icons/clock.png" },
+  { name: "Sticky Notes", icon: "/icons/sticky-note.png" },
+  { name: "Photos", icon: "/icons/gallery.png" },
+  { name: "Camera", icon: "/icons/camera.png" },
+  { name: "Notepad", icon: "/icons/notepad.png" },
 ];
 
 const recommendedFiles = [
@@ -35,7 +35,7 @@ const StartMenu = ({ closeMenu }: { closeMenu: () => void }) => {
   }, []);
 
   return (
-    <div className="start-menu absolute bottom-12 left-4 w-[650px] start-button text-white rounded-lg shadow-lg p-4">
+    <div className="start-menu absolute bottom-12 left-4 w-[650px] start-button text-white rounded-lg shadow-lg py-4 px-10">
       {/* Search Bar */}
       <div className="relative py-5">
         <FaSearch className="absolute left-4 bottom-[39%] text-gray-400" />
@@ -47,8 +47,11 @@ const StartMenu = ({ closeMenu }: { closeMenu: () => void }) => {
       </div>
 
       {/* Pinned Apps */}
-      <div className="mt-4">
-        <h3 className="text-sm text-gray-400">Pinned</h3>
+      <div className="py-4">
+        <div className="flex items-center justify-between py-3">
+        <h3 className="text-sm text-white font-extralight">Pinned</h3>
+        <button className="text-xs small-buttons py-1 font-thin px-2 rounded text-white border border-gray-700">All&nbsp;&nbsp;&nbsp;{`>`}</button>
+        </div>
         <div className="grid grid-cols-6 gap-3 mt-2">
           {pinnedApps.map((app) => (
             <div key={app.name} className="flex flex-col items-center text-center">
