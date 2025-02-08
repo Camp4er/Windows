8d4600cc5a7094f0a9d6fe7d80d70634fd0ee163
Camp4er <camp4ercode@gmail.com>
2025-02-09 02:07:42 +0530

    icon alignmnet in desktop

--- src/components/DesktopIcon.tsx
+++ src/components/DesktopIcon.tsx
@@ -6,8 +6,8 @@ type DesktopIconProps = {
   
   export default function DesktopIcon({ title, icon, onClick }: DesktopIconProps) {
     return (
-      <div onClick={onClick} className="flex flex-col items-center cursor-pointer text-center p-1 px-2 rounded hover:bg-gray-800 ">
-        <div className="w-12 h-12 flex items-center justify-center rounded-md">
+      <div onClick={onClick} className="flex flex-col  cursor-pointer text-center p-1 px-2 rounded hover:bg-gray-800 ">
+        <div className="w-12 h-12 flex  justify-center rounded-md">
           <img src={icon} alt={`${title} icon`} className="w-10 h-10" />
         </div>
         <span className="mt-1 text-xs">{title}</span>
